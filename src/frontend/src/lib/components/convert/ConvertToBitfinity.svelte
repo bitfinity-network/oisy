d9b02af5b948ebf9bbcabf7caee2b54b7d6ec4a6
assetcorp <sondelalimic@gmail.com>
2025-01-13 09:25:37 +0000

    chore: remove unused imports

--- src/frontend/src/lib/components/convert/ConvertToBitfinity.svelte
+++ src/frontend/src/lib/components/convert/ConvertToBitfinity.svelte
@@ -1,20 +1,13 @@
 <script lang="ts">
-	import { isNullish, nonNullish } from '@dfinity/utils';
-	import { selectedEthereumNetwork } from '$eth/derived/network.derived';
-	import { ethereumToken, ethereumTokenId } from '$eth/derived/token.derived';
+	import { nonNullish } from '@dfinity/utils';
 	import ConvertBTF from '../../../btf/send/ConvertBTF.svelte';
 	import IconCkConvert from '../icons/IconCkConvert.svelte';
 	import { tokenWithFallback } from '$lib/derived/token.derived';
-	import { i18n } from '$lib/stores/i18n.store';
 	import { isRequiredTokenWithLinkedData } from '$lib/utils/token.utils';
 	import type { RequiredTokenWithLinkedData } from '$lib/types/token';
 	import { modalConvertToTwinToken } from '$lib/derived/modal.derived';
-	import { ICP_NETWORK } from '$env/networks.env';
 	import { ckEthHelperContractAddress } from '$icp-eth/derived/cketh.derived';
 	import BTFSendTokenModal from '../../../btf/send/BTFSendTokenModal.svelte';
-	import { ethAddressNotLoaded } from '$lib/derived/address.derived';
-	import { ckEthMinterInfoStore } from '$icp-eth/stores/cketh.store';
-	import { networkICP } from '$lib/derived/network.derived';
 	import { setContext } from 'svelte';
 	import { initSendContext, SEND_CONTEXT_KEY } from '$lib/stores/send.store';
 
